f2ef762ab7a97548e85af8b1a9f31b66ebcbe604
Fernando <fernando.leitao@gmail.com>
2025-09-15 22:05:00 -0300

    refactor(scraping): Reestrutura e aprimora o módulo de coleta de dados
    feat: Integra a execução condicional do scraping na inicialização da aplicação Flask
    
    - Reorganiza a lógica de scraping em um novo pacote `src/services/scraping`.
    - Renomeia `scraping.py` para `core.py` e `run_scraping.py` para `file_handler.py`.
    - Move o diretório `data` de `src/data` para a raiz do projeto.
    - Implementa injeção de dependência para a função `update_books_data` em `file_handler.py`.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from pathlib import Path
 
 from extensions import db
 from conf import Conf
 from routes import register_blueprints
+from services.scraping.core import scrape_all_books
+from services.scraping.file_handler import update_books_data
+
 
 def create_app(config_object=Conf):
     app = Flask(__name__, instance_relative_config=True)
@@ -10,6 +14,13 @@ def create_app(config_object=Conf):
 
     db.init_app(app)
     register_blueprints(app)
+
+    # Provisoriamente chamado aqui para garantir que o arquivo CSV exista
+    with app.app_context():
+        project_root = Path(__file__).parent.parent
+        output_file = project_root / "data" / "scraped_books.csv"
+        if not output_file.exists():
+            update_books_data(output_file, scraper_function=scrape_all_books)
 
     return app
 
