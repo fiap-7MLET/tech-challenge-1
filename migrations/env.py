d3c4210dbb8a3f3dafa17f4e17e8b4c7a175c08a
Nhai Moura <nhaiarar@gmail.com>
2025-09-20 16:06:42 +0200

    Revert "Merge branch 'main' of https://github.com/fiap-7MLET/tech-challenge-1"
    This reverts commit 0231a573fbc31acd4b49fc8b682fea95e779b374, reversing
    changes made to f2a9c688a8111b34a7e09b11c525700384f6b281.

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
